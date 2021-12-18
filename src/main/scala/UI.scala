a72031d51304c192875093f0a9e32d3645cf7276
Matias Södersved <matiassodersved2@gmail.com>
2021-12-18 23:24:56 +0200

    Add 9 UI-buttons that do nothing (yet)

--- src/main/scala/UI.scala
+++ src/main/scala/UI.scala
@@ -1,8 +1,9 @@
 import scalafx.application.JFXApp3
-import scalafx.geometry.Insets
+import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
+import scalafx.scene.control.Button
 import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
+import scalafx.scene.layout.{HBox, VBox}
 import scalafx.scene.paint.Color._
 import scalafx.scene.paint._
 import scalafx.scene.text.Text
@@ -24,34 +25,60 @@ object UI extends JFXApp3 {
       case Some(winner) => println("Winner:", winner.name)
       case None => println("Tied")
     }
+    val buttons = new Array[Button](9)
+    for (i <- buttons.indices){
+      buttons(i) = new Button {
+        minWidth = 80
+        maxWidth = 80
+        minHeight = 80
+        maxHeight = 80
+        text = i.toString
+      }
+    }
+    val HBoxes = new Array[HBox](3)
+    for (i <- HBoxes.indices){
+      HBoxes(i) = new HBox {
+        alignment = Pos.Center
+        padding = Insets(10, 10, 10, 10)
+        spacing = 10
+        children = buttons.grouped(3).toSeq(i)
+      }
+    }
     stage = new JFXApp3.PrimaryStage {
       //    initStyle(StageStyle.Unified)
       title = "ScalaFX Hello World"
       scene = new Scene {
         fill = Color.rgb(38, 38, 38)
-        content = new HBox {
+        content = new VBox {
+          alignment = Pos.Center
           padding = Insets(50, 80, 50, 80)
           children = Seq(
-            new Text {
-              text = "Hello"
-              style = "-fx-font: normal bold 100pt sans-serif"
-              fill = new LinearGradient(
-                endX = 0,
-                stops = Stops(Green, DarkRed))
-            },
-            new Text {
-              text = "World"
-              style = "-fx-font: italic bold 100pt sans-serif"
-              fill = new LinearGradient(
-                endX = 0,
-                stops = Stops(Blue, DarkGray)
+            new HBox {
+              padding = Insets(10, 10, 10, 10)
+              children = Seq(
+                new Text {
+                  text = "TicTacToe"
+                  style = "-fx-font: normal bold 70pt sans-serif"
+                  fill = new LinearGradient(
+                    endX = 0,
+                    stops = Stops(Green, DarkRed))
+                },
+                new Text {
+                  text = "made by Matias"
+                  style = "-fx-font: italic bold 40pt sans-serif"
+                  fill = new LinearGradient(
+                    endX = 0,
+                    stops = Stops(Blue, DarkGray)
+                  )
+                  effect = new DropShadow {
+                    color = DarkGray
+                    radius = 20
+                    spread = 0.30
+                  }
+                }
               )
-              effect = new DropShadow {
-                color = DarkGray
-                radius = 20
-                spread = 0.30
-              }
-            }
+            },
+            HBoxes(0), HBoxes(1), HBoxes(2)
           )
         }
       }
